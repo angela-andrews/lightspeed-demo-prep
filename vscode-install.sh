b83cbcaa112e000e73c31b78cd41db176ffc90c5
Angela Andrews <38594234+angela-andrews@users.noreply.github.com>
2023-08-10 11:43:58 -0400

    added git config settings

--- vscode-install.sh
+++ vscode-install.sh
@@ -5,3 +5,5 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
 dnf install code
+git config --global user.email "38594234+angela-andrews@users.noreply.github.com"
+git config --global user.name "Angela Andrews"
